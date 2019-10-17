360ffe4d7382a6f26bb30646529257bc5c4f4675
Perry Trinh <perry.trinh@berkeley.edu>
2019-10-16 21:51:51 -0700

    Adding auth functionality

---
+++ src/components/navigation/MainNavigation.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import AuthContext from "../../context/auth-context";
+import "./MainNavigation.css";
+
+const mainNavigation = props => (
+  <AuthContext.Consumer>
+    {(context) => {
+      return (
+        <header className="main-navigation">
+          <div className="main-navigation__logo">
+            <h1>EasyEvent</h1>
+          </div>
+          <nav className="main-navigation__items">
+            <ul>
+              {!context.token && <li>
+                <NavLink to="/auth">Auth</NavLink>
+              </li>}
+              <li>
+                <NavLink to="/events">Events</NavLink>
+              </li>
+              {context.token && <li>
+                <NavLink to="/bookings">Bookings</NavLink>
+              </li>}
+            </ul>
+          </nav>
+        </header>
+      );
+    }}
+  </AuthContext.Consumer>
+);
+
+export default mainNavigation;
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,12 +1,95 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import AuthContext from '../context/auth-context';
+import './Auth.css';
 
 class AuthPage extends Component {
-    render() {
-        return (
-            <h1>This is the Auth Page</h1>
-        );
-    }
-}
+  state = {
+    isLoggedIn: true
+  };
 
+  static contextType = AuthContext;
+
+  constructor(props) {
+    super(props);
+    this.emailElem = React.createRef();
+    this.passwordElem = React.createRef();
+  }
+
+  submitHandler = event => {
+    event.preventDefault();
+    const email = this.emailElem.current.value;
+    const password = this.passwordElem.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    const requestBody = this.state.isLoggedIn ? {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    } : {
+      query: `
+        mutation {
+          createUser(userInput: {email: "${email}", password: "${password}"}) {
+            _id
+            email
+          }
+        }
+      `
+    };
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      if (res.status !== 200 && res.status !== 201) {
+        throw new Error(`Failed HTTP Request: ${res.status}`);
+      }
+      return res.json();
+    }).then(responseData => {
+      if (responseData.data.login.token) {
+        const serverRes = responseData.data.login;
+        this.context.login(serverRes.token, serverRes.userId, serverRes.tokenExpiration);
+        
+      }
+    }).catch(error => {
+      console.log(`Error sending HTTP request: ${error}`);
+    })
+  }
+
+  switchModeHandler = () => {
+    this.setState(prevState => {
+      return {isLoggedIn: !prevState.isLoggedIn};
+    })
+  }
+
+  render() {
+    return <form className="auth-form" onSubmit={this.submitHandler}>
+      <div className="form-control">
+        <label htmlFor="email">E-Mail</label>
+        <input type="email" id="email" ref={this.emailElem}/>
+      </div>
+      <div className="form-control">
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" ref={this.passwordElem}/>
+      </div>
+      <div className="form-actions">
+        <button type="submit">Submit</button>
+        <button type="button" onClick={this.switchModeHandler}>Switch to {this.state.isLoggedIn ? 'Signup' : 'Login'}</button>
+      </div>
+    </form>;
+  }
+}
 
 export default AuthPage;
